aa094db99d6d0d9aca0ab0cc2a9a21a876343366
BapujiTate <84676757+BapujiTate@users.noreply.github.com>
2021-08-21 10:28:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 (function () {
 
-var names = ["Yaakov", "Ravi", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Jim"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Larry", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
