13a033363093516addc96c38edc024c764019053
BapujiTate <84676757+BapujiTate@users.noreply.github.com>
2021-08-21 10:16:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+(function () {
+
+var names = ["Bapuji", "Ravi", "Jerry", "Tom", "Siri", "Alexa", "Cortana", "Pranav", "Shivani", "Sumedha"];
+
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+  }
+  else {
+    helloSpeaker(names[i]);
+  }
+}
+
+})();
